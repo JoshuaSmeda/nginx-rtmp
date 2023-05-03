01686371ff1d0f57af5d09bc18ec5a894b20cbda
Joshua Smeda <smeda.josh14@gmail.com>
2023-05-03 23:00:08 +0200

    Delete pre-start.sh

--- ec2/pre-start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Create a policy that allows access to your ECR repository, assign it to a Role
-# Apply that Role to the EC2 instance 
-
-# Authenticate with AWS ECR and pull the nginx-rtmp image
-aws ecr get-login-password --region af-south-1 | docker login --username AWS --password-stdin 215371600831.dkr.ecr.af-south-1.amazonaws.com
-docker image pull 215371600831.dkr.ecr.af-south-1.amazonaws.com/nginx-rtmp:latest
